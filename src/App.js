c34e7aeba8fd052b665eb4ea640b0a0b4943f1d9
Russ‘s MacBook <albert@MacBook-Pro.local>
2023-03-28 21:21:35 +0800

    shalloe merge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: {firstname:'Russ', lastname: "Deng"},
+      company: 'Apple'
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name.firstname} {this.state.name.lastname}, I am working at {this.state.company}
+          </p>
+          <button 
+            onClick={() => {
+              this.setState({name: {firstname:'Albert', lastname: "Deng"}});
+              console.log(this.state);
+            }}
+          >
+            change name
+          </button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+    
+  }
+  
 
 export default App;
