ff3c3728ea8c5ffb976f52580fcb81ca43b544e2
Russ‘s MacBook <albert@MacBook-Pro.local>
2023-04-03 22:05:22 +0800

    reform components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 
 import logo from './logo.svg';
 import './App.css';
+import CardList from './components/card-list/card-list.component'
 
 class App extends Component {
   constructor(){
@@ -50,14 +51,16 @@ class App extends Component {
               onChange={onSearchChange}
       />
 
-       {filteredMonsters.map((monster) => {
+       {/* {filteredMonsters.map((monster) => {
         return( 
           <div key={monster.id}>
             <h1>{monster.name}</h1>
           </div>
           );
         })
-       }
+       } */}
+
+       <CardList />
         
       </div>
     )
